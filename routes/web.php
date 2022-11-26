11492cdbb19f99821da863719192816d160e12d9
廖文忠 <wcliao1962@gmail.com>
2022-11-26 14:53:01 +0800

    用個別的Route::敘述一一宣告路由

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,13 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::resource('photos', PhotoController::class)->only([
-    'index', 'show', 'store', 'update', 'destroy'
-]);
+//Route::resource('photos', PhotoController::class)->only([
+//    'index', 'show', 'store', 'update', 'destroy'
+//]);
+
+Route::get('photos',[PhotoController::class,'index'])->name('photos.index');
+Route::get('photos/{photo}',[PhotoController::class,'show'])->name('photos.show');
+Route::post('photos',[PhotoController::class,'store'])->name('photos.store');
+Route::patch('photos/{photo}',[PhotoController::class,'update'])->name('photos.update');
+Route::delete('photos/{photo}',[PhotoController::class,'destroy'])->name('photos.destroy');
 
