c340207ad509053f36694241e8433284e5dfb80f
廖文忠 <wcliao1962@gmail.com>
2022-11-26 14:19:11 +0800

    加入destroy路由

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,6 @@ Route::middleware([
 });
 
 Route::resource('photos', PhotoController::class)->only([
-    'index', 'show', 'store', 'update'
+    'index', 'show', 'store', 'update', 'destroy'
 ]);
 
